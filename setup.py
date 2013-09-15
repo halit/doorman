2dd10f4ecdf7d150c12f9fae42d92815ff95cff7
Halit Alptekin <info@halitalptekin.com>
2013-09-15 23:54:26 +0300

    hotfix readme

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ config = {
     'author': 'Halit Alptekin.com',
     'url': '',
     'author_email': 'info@halitalptekin.com',
-    'version': '0.1.0',
+    'version': '0.1.1',
     'install_requires': ['nose'],
     'packages': ['doorman'],
     'scripts': [],
