0eda5dfdd6e3382da06d348cf9668ee3dc33199b
Halit Alptekin <info@halitalptekin.com>
2013-09-15 19:55:14 +0300

    write first code

--- doorman/doorman.py
+++ doorman/doorman.py
@@ -1,1 +1,49 @@
-__author__ = 'halit'
+from ConfigParser import ConfigParser
+
+
+class Config:
+    def __init__(self, config_file):
+        self.config_file = config_file
+
+    def parse(self):
+        config = ConfigParser()
+        config.read([self.config_file])
+
+        configs = dict()
+        config_secrets = dict()
+
+        for key, value in config.items('secrets'):
+            config_secrets[key] = value
+
+        for key, value in config.items('files'):
+            configs[key] = dict(secret_thing=config_secrets[key],
+                                secret_file=value)
+
+        return configs
+
+
+class Doorman:
+    def __init__(self, status, config_file):
+        self.status = status
+        self.config = Config(config_file).parse()
+
+    def run(self):
+        if self.status:
+            self.hide()
+        else:
+            self.unhide()
+
+    def open_and_replace(self, file_path, old, new):
+        with open(file_path, "w") as f:
+            full_text = f.read()
+            full_text = full_text.replace(old, new)
+            f.write(full_text)
+
+    def hide(self):
+        for key, value in self.config.items():
+            secret_thing, secret_file = value.items()
+            self.open_and_replace(secret_file[1], secret_thing[1], secret_thing[1])
+
+
+    def unhide(self):
+        pass
--- doorman/main.py
+++ doorman/main.py
@@ -1,16 +1,18 @@
 import argparse
+import os
+from doorman import Doorman
 
 parser = argparse.ArgumentParser(description='Doorman keeps your secret things')
-parser.add_argument('-s','--secret', action="store_true", dest="status", help='Hide all secret things')
-parser.add_argument('-u','--unsecret', action="store_false", dest="status", help='Unhide all secret things')
+parser.add_argument('-s', '--secret', action="store_true", dest="status", help='Hide all secret things')
+parser.add_argument('-u', '--unsecret', action="store_false", dest="status", help='Unhide all secret things')
+parser.add_argument('-c', '--config', action="store", dest="config_file", type=file, help='Config file')
 
 args = parser.parse_args()
 
+
 def main():
-    if args.status:
-        print "Hide all things!"
-    else:
-        print "Unhide all things!"
+    doorman = Doorman(args.status, os.path.abspath(args.config_file.name))
+    doorman.run()
 
 if __name__ == "__main__":
     main()
