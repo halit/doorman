fff4a3d699355567107bad14046ca3d4fc6c73e1
Halit Alptekin <info@halitalptekin.com>
2013-09-15 23:11:12 +0300

    Edit cli file for new config style

--- doorman/main.py
+++ doorman/main.py
@@ -3,20 +3,19 @@ import os
 from doorman import Doorman
 
 DEFAULT_CONFIG_PATH = os.path.join(os.path.expanduser("~"), ".doormanrc")
-DEFAULT_CONFIG = """[secrets]
-test_secret =
-
-[files]
-test_secret =
-"""
+DEFAULT_CONFIG = """test_secret >> my secret thing >> test_file"""
 
 if not os.path.exists(DEFAULT_CONFIG_PATH):
     with open(DEFAULT_CONFIG_PATH, "w") as f:
         f.write(DEFAULT_CONFIG)
 
+
+def is_default_config():
+    return open(DEFAULT_CONFIG_PATH, "r").read() == DEFAULT_CONFIG
+
 parser = argparse.ArgumentParser(description='Doorman keeps your secret things')
 parser.add_argument('-s', '--secret', action="store_true", dest="status", help='Hide all secret things')
-parser.add_argument('-u', '--unsecret', action="store_false", dest="status", help='Unhide all secret things')
+parser.add_argument('-u', '--unsecret', action="store_false", dest="status", help='Open all secret things')
 parser.add_argument('-c', '--config', action="store", dest="config_file",
                     default=DEFAULT_CONFIG_PATH, type=file, help='Config file')
 args = parser.parse_args()
@@ -26,11 +25,19 @@ def main():
     """
     Main function
     """
-    if args.config_file.name is DEFAULT_CONFIG_PATH:
-        parser.print_help()
-    else:
+
+    if not is_default_config():
+        print "# running" + args.config_file.name + " config file"
         doorman = Doorman(args.status, os.path.abspath(args.config_file.name))
+
+        if args.status:
+            print "# hide all secret things"
+        else:
+            print "# open all secret things"
+
         doorman.run()
+    else:
+        parser.print_help()
 
 
 if __name__ == "__main__":
