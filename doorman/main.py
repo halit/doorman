721b7c67df67937ad453cbc9310d73eef5b9402a
Gerben Oolbekkink <1284666+halit@users.noreply.github.com>
2014-06-07 10:19:18 +0200

    Created a mutually exclusive argument group for the arguments secret and unsecret. Secret is now the default option. Moved the config file from ~/.doormanrc.yml to ~/.config/doorman.yml.

--- doorman/main.py
+++ doorman/main.py
@@ -2,7 +2,7 @@ import argparse
 import os
 from doorman import Doorman, DoormanException
 
-DEFAULT_CONFIG_PATH = os.path.join(os.path.expanduser("~"), ".doormanrc.yml")
+DEFAULT_CONFIG_PATH = os.path.join(os.path.expanduser("~"), ".config/doorman.yml")
 DEFAULT_CONFIG = """test_file:
  test_secret: my secret thing"""
 
@@ -15,10 +15,12 @@ def is_default_config():
     return open(DEFAULT_CONFIG_PATH, "r").read() == DEFAULT_CONFIG
 
 parser = argparse.ArgumentParser(description='Doorman keeps your secret things')
-parser.add_argument('-s', '--secret', action="store_true", dest="status", help='Hide all secret things')
-parser.add_argument('-u', '--unsecret', action="store_false", dest="status", help='Open all secret things')
+parser.set_defaults(status=True)
+group = parser.add_mutually_exclusive_group(required=False)
+group.add_argument('-u', '--unsecret', action="store_false", dest="status", help='Open all secret things')
+group.add_argument('-s', '--secret', action="store_true", dest="status", help='Hide all secret things')
 parser.add_argument('-c', '--config', action="store", dest="config_file",
-                    default=DEFAULT_CONFIG_PATH, type=file, help='Config file')
+                    default=DEFAULT_CONFIG_PATH, help='Config file')
 args = parser.parse_args()
 
 
@@ -29,10 +31,10 @@ def main():
 
     if not is_default_config():
         try:
-            doorman = Doorman(args.status, os.path.abspath(args.config_file.name))
+            doorman = Doorman(args.status, os.path.abspath(args.config_file))
             doorman.run()
         except DoormanException, e:
-            print e, '\n'
+            print(e, '\n')
             parser.print_help()
     else:
         parser.print_help()
